ca4a03a7a30914f78fc2b66a638372c3aaa8db40
Pablo Ortiz López <pabloortiz132@gmail.com>
2023-10-07 23:05:45 +0200

    Figured out that i forgot about the ENABLE pin

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -3,38 +3,50 @@
 
 using namespace std;
 
-const int chipSelectPin = 10; // Chip select (CS) pin
+#define CSN_MAX7060 10
+#define ENABLE 8
 
 void setup() {
   // Initialize SPI
   SPI.begin();
-  
+  SPI.setClockDivider(SPI_CLOCK_DIV128);  
   // Initialize Serial communication
-  Serial.begin(9600); // You can change the baud rate as needed
+  Serial.begin(300); // You can change the baud rate as needed
   
   // Set chipSelectPin as an output
-  pinMode(chipSelectPin, OUTPUT);
-  
-  // Invert chip select to select the device
-  digitalWrite(chipSelectPin, LOW);
-
-  // Send command to read from address 0
-  uint8_t read_command = 0x02; // Modify this command as needed
-  SPI.transfer(read_command);
-
-  // Read the value at address 0
-  uint8_t value = SPI.transfer(0x00); // You may send a dummy byte here
-
-  // Release the device by setting chipSelectPin high
-  digitalWrite(chipSelectPin, HIGH);
-
-  // Print the value to the Serial monitor
-  Serial.print("Value at address 0: ");
-  Serial.println(value);
-
-  // Now 'value' contains the byte read from address 0
+  pinMode(CSN_MAX7060, OUTPUT);
+  pinMode(ENABLE, OUTPUT);
+  digitalWrite(ENABLE,HIGH);
+  delay(2);
 }
 
 void loop() {
-  // Your main code here
+  
+  // Invert chip select to select the device
+  digitalWrite(CSN_MAX7060, LOW);
+
+  // Send command
+  SPI.transfer(0x02);
+
+  // Read address 0
+  SPI.transfer(0x00);
+
+  // Read value
+  uint8_t signature = SPI.transfer(0x04);
+  uint8_t tmux = SPI.transfer(0x12);
+  uint8_t status = SPI.transfer(0x00);
+
+  // Release the device by setting CS high
+  digitalWrite(CSN_MAX7060, HIGH);
+
+  // Print the value to the Serial monitor
+  Serial.print("Signature: ");
+  Serial.println(signature);
+  Serial.print("tmux: ");
+  Serial.println(tmux);
+  Serial.print("status: ");
+  Serial.println(status);
+
+  // Now 'value' contains the byte read from address 0
+  delay(2000);
 }
